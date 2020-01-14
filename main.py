c2ebc09c5b610b9878858302083ca37fc377c638
CT15 <calvin.tantio@outlook.com>
2020-01-14 13:47:15 +0800

    Move data preprocessing to preprocess.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import pandas as pd
 import numpy as np
 
-from text_processing import process_text
 from train_and_eval import train_and_eval
 
-data = pd.read_csv('data/result.csv', comment='#')
+data = pd.read_csv('data/result_large.csv', comment='#')
 
 thread_ids = np.array(data.thread_id)
-
-posts = data.posts.map(lambda x: process_text(x))
-posts = posts.replace([''], '<html>')
 posts = np.array(posts)
-
 labels = np.array(data.labels)
-labels[labels == 'Instructor'] = '1'
-labels[labels == 'Student'] = '0'
-labels = labels.astype(np.int)
 
 train_and_eval(thread_ids=thread_ids, posts=posts, labels=labels, 
                max_posts=20, max_words=400, frac=[0.8, 0.1, 0.1], seed=0,
--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,35 @@
 import pandas as pd
-from PostLabeller import PostLabeller, FlattenerType, InterventionModelType
+import numpy as np
 
+from PostLabeller import PostLabeller, FlattenerType, InterventionModelType
+from text_processing import process_text
+
+print('Reading csv')
 user_df = pd.read_csv('data/user.csv', comment='#')
 post_df = pd.read_csv('data/post.csv', comment='#')
 comment_df = pd.read_csv('data/comment.csv', comment='#')
 
+print('Creating post labeller')
 pl = PostLabeller(user_df, post_df, comment_df,
                   FlattenerType.TIMESTAMP, InterventionModelType.GIM)
 
+print('Preprocessing post texts')
+posts = pd.Series(pl.posts)
+posts = posts.map(lambda x: process_text(x))
+posts = posts.replace([''], '<html>')
+pl.posts = posts.to_numpy()
+
+def relabel(x):
+    if x == 'Instructor':
+        return 1
+    elif x == 'Student':
+        return 0
+    else:
+        raise Exception('Wut!!!')
+
+print('Relabeling labels')
+vrelabel = np.vectorize(relabel)
+pl.labels = vrelabel(pl.labels)
+
+print('Saving to csv')
 pl.to_csv('data/result_large.csv')
