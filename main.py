bba5cf37df1bfc78acfa0e674f532f2f2a1eeb89
CT15 <calvin.tantio@outlook.com>
2020-01-15 02:12:19 +0800

    Defensive

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from train_and_eval import train_and_eval
 
 data = pd.read_csv('data/result_large.csv', comment='#')
 
-thread_ids = np.array(data.thread_id)
-posts = np.array(data.posts)
-labels = np.array(data.labels)
+thread_ids = np.array(data.thread_id).astype(str)
+posts = np.array(data.posts).astype(str)
+labels = np.array(data.labels).astype(int)
 
 train_and_eval(thread_ids=thread_ids, posts=posts, labels=labels, 
                max_posts=20, max_words=400, frac=[0.8, 0.1, 0.1], seed=0,
